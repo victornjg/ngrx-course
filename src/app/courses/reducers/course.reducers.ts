563992ae07683ed8a7f9968d96a4bb4aaabe08a7
Victor Gomes <victor.nnjg@gmail.com>
2021-07-13 15:16:21 -0300

    setting a sort comparer to the courses entity adapter

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,14 +1,16 @@
 import { createEntityAdapter, EntityState } from '@ngrx/entity';
 import { createReducer, on } from '@ngrx/store';
 import { CoursesActions } from '../action-types';
-import { Course } from '../model/course';
+import { compareCourses, Course } from '../model/course';
 
 
 export interface CoursesState extends EntityState<Course> {
 
 }
 
-export const adapter = createEntityAdapter<Course>();
+export const adapter = createEntityAdapter<Course>({
+  sortComparer: compareCourses
+});
 
 export const initialCoursesState = adapter.getInitialState();
 
